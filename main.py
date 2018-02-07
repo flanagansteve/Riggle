e2af7ba8e2deb383b25f38e0b6178241a90f5699
sflanagan <sflanagan@mitre.org>
2018-02-07 11:00:51 -0500

    updated dependency list

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from setupDevnetAndDeploy import *
     # geth
     # Solc
     # py-solc
-    # tkinter
-        # brew install python3-tk
+    # possibly tkinter?
         # sudo apt-get install python3-tk
+    # possibly subprocess?
 #TODO: set path variables for geth and solc
-    # windows: setx
+    # windows: setx or even just set
     # osx/linux: edit bash profile
-#TODO:
 init()
 defineContractObject()
 instantiateContractObject(0)
